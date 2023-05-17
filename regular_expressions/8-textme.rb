e6b2132410ad524e55776d957e4dd158e3afa0b9
eumunyana <e.umunyana@alustudent.com>
2023-05-17 09:00:50 -0700

    Textme

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
-sender = ARGV[0].scan(/sender:(.*?)\]/)
-receiver = ARGV[0].scan(/receiver:(.*?)\]/)
+
+from = ARGV[0].scan(/from:(.*?)\]/)
+to = ARGV[0].scan(/to:(.*?)\]/)
 flags = ARGV[0].scan(/flags:(.*?)\]/)
 puts [sender, receiver, flags].join(',')
